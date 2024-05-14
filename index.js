56ff963c6c6cc791345af38ef5616bf945dc4e32
konda ravilla <kondaravilla000@gmail.com>
2024-05-14 22:17:41 +0530

    operators

--- index.js
+++ index.js
@@ -23,65 +23,182 @@
 
 
 
-var msg = "sachin"
+// var msg = "sachin"
 
-console.log(msg, typeof msg)
+// console.log(msg, typeof msg)
 
-var num = 100
-console.log(num, typeof num)
+// var num = 100
+// console.log(num, typeof num)
 
-var isLoggedIn = false
+// var isLoggedIn = false
 
-console.log(isLoggedIn, typeof isLoggedIn)
+// console.log(isLoggedIn, typeof isLoggedIn)
 
-var color 
+// var color
 
-console.log(color, typeof color)
+// console.log(color, typeof color)
 
-var height = null
+// var height = null
 
-console.log(height, typeof height)
+// console.log(height, typeof height)
 
-//reference
+// //reference
 
-var obj = {
-  name: 'sachin',
-  age: 40
-}
-console.log(obj, typeof obj)
+// var obj = {
+//   name: 'sachin',
+//   age: 40
+// }
+// console.log(obj, typeof obj)
 
-var arr = [1, 2, 3, 4, 5]
-console.log(arr, typeof arr)
-console.log(Array.isArray(arr))
+// var arr = [1, 2, 3, 4, 5]
+// console.log(arr, typeof arr)
+// console.log(Array.isArray(arr))
 
-var fun1 = () => {
-  var x = 100
-}
+// var fun1 = () => {
+//   var x = 100
+// }
 
-console.log(fun1, typeof fun1)
+// console.log(fun1, typeof fun1)
 
-fun1()
+// fun1()
 
 
-//storage pattern
+// //storage pattern
 
-// primitives
+// // primitives
 
-var a = 20
-console.log(a)
+// var a = 20
+// console.log(a)
 
-var b = a
+// var b = a
 
-b = 40
-console.log(b)
+// b = 40
+// console.log(b)
 
-//referenece
+// //referenece
 
-var obj1 = {
-  value: 10
-}
+// var obj1 = {
+//   value: 10
+// }
 
-var obj2 = obj1
+// var obj2 = obj1
+
+
+// Operators
+
+
+//Arithmatic
+
+
+// var a = 20
+
+// var b = 10
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a ** b)
+// console.log(a++)
+// console.log(++a)
+// console.log(--a)
+// console.log(a--)
+// console.log(a)
+
+
+// Assignment operator
+
+// var x = 10
+
+// console.log(x)
+
+// console.log(x += 5)
+// console.log(x -= 5)
+// console.log(x *= 5)
+// console.log(x /= 5)
+// console.log(x %= 5)
+// console.log(x **= 5)
+
+
+//Comparision operators
+
+// var x = 10
+// var x = undefined
+
+// var y = 11
+// var y = 10
+// var y = '10'
+
+// var y = null
+
+// var x = {value: 10}
+// var y = {value: 10}
+// var x = {value: 10}
+// var y = {value: 20}
+// var x = {}
+// var y = {}
+
+// var x = {}
+
+// var y = x
+
+// var x = [1,2,3]
+// var y = [1,2,3]
+// var x = [1,2,3]
+// var y = [4,5,6]
+// var x = []
+// var y = []
+
+// var x = []
+
+// var y = x
+
+
+
+//relational
+
+// console.log(x>y)
+// console.log(x>=y)
+// console.log(x<y)
+// console.log(x<=y)
+
+// equality operators
+
+// console.log(x==y) //loose equality (compares only value)
+// console.log(x===y) //strict equality (compares value and its data type)
+
+// Ternary operator
+
+
+// var age = 18
+
+// var msg = age >= 18 ? 'having vote' : 'not having vote'
+
+// console.log(msg)
+
+
+//Logical operators
+
+
+var income = true
+
+var cibilScore = false
+
+
+// Logical AND
+// var hdfcCreditCard = income && cibilScore
+// console.log(hdfcCreditCard)
+
+// Logical OR
+// var rblCreditCard = income || cibilScore
+// console.log(rblCreditCard)
+
+
+// Not operator 
+// console.log(!income)
+
+
 
 
 
