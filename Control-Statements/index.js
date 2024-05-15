c4300be2c65975b7d8412bbe3890e085ff586d96
konda ravilla <kondaravilla000@gmail.com>
2024-05-15 22:44:01 +0530

    control-statements

---
+++ Control-Statements/index.js
@@ -0,0 +1,134 @@
+
+
+
+// var marks = 80
+
+//if else
+
+// if (marks < 35) {
+//   console.log('Fail')
+// } else if (marks >=35 && marks <60) {
+//   console.log('Second class')
+// } else if (marks >= 60 && marks < 75) {
+//   console.log('First class')
+// } else {
+//   console.log('Distinction')
+// }
+
+
+// var marks = 50
+
+// switch (marks) {
+//   case marks < 35:
+//     console.log('Fail')
+//     break;
+//   case marks >= 35 && marks < 60:
+//     console.log('Second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('First class')
+//     break;
+//   default:
+//     console.log('Distinction')
+    
+// }
+
+
+// var num = 0
+
+// if alse
+
+// if (num > 0) {
+//   console.log('Positive Number')
+// } else if (num < 0) {
+//   console.log('Negative number')
+// } else {
+  //   console.log('Zero')
+  // }
+  
+  // switch case
+  
+// var num = -10
+  
+// switch (num) {
+//   case num > 0:
+//     console.log('Positive number');
+//     break;
+//   case num < 0:
+//     console.log('Negative number')
+//     break;
+//   default:
+//     console.log('Zero')
+// }
+
+
+// var customer = 'MONGO'
+
+// if (customer === 'GOLD') {
+//   console.log('Gold customer')
+// } else if (customer === 'SILVER') {
+//   console.log('Silver customer')
+// } else if (customer === 'IRON') {
+//   console.log('Iron customer')
+// } else {
+//   console.log('Unknown customer')
+// }
+
+// var customer = 'GOLD'
+
+// switch (customer) {
+//   case 'GOLD':
+//     console.log('Gold customer')
+//     break;
+//   case 'SILVER':
+//     console.log('Silver cutomer')
+//     break;
+//   case 'IRON':
+//     console.log('Iron customer');
+//     break;
+//   default:
+//     console.log('Unknown customer')
+// }
+
+// var dayOfWeek = 7
+
+// if (dayOfWeek === 1) {
+  //   console.log('Monday')
+  // } else if (dayOfWeek === 2) {
+    //   console.log('Thesday')
+// } else if (dayOfWeek ===3) {
+  //   console.log('wednesday')
+// } else if (dayOfWeek === 4) {
+//   console.log('Thursday')
+// } else if (dayOfWeek ===5) {
+//   console.log('Friday')
+// } else if (dayOfWeek ===6) {
+  //   console.log('Saturday')
+  // } else {
+    //   console.log('Sunday')
+    // }
+    
+// var dayOfWeek = 7
+
+// switch (dayOfWeek) {
+//   case 1:
+//     console.log('Monday')
+//     break;
+//   case 2:
+//     console.log('Thesday')
+//     break;
+//   case 3:
+//     console.log('Wednesday')
+//     break;
+//   case 4:
+//     console.log('Thursday');
+//     break;
+//   case 5:
+//     console.log('Friday')
+//     break;
+//   case 6:
+//     console.log('Saturday');
+//     break;
+//   default:
+//     console.log('Sunday')
+// }
