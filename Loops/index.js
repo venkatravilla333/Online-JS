39f1888f6256a16e3873c6fc9fe06f629bcdfe47
konda ravilla <kondaravilla000@gmail.com>
2024-05-16 23:21:38 +0530

    loops

---
+++ Loops/index.js
@@ -0,0 +1,202 @@
+var name = 'kohli'
+
+
+//without using loops
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+//with using loops
+
+// for loop
+
+// for (var i = 101; i <= 100; i++){
+//   console.log(i, name)
+// }
+
+//while loop
+
+// var i = 101
+
+// while (i<=100) {
+//   console.log(i, name)
+//   i++
+// }
+
+//do while loop
+
+// var i = 101
+
+// do {
+//   console.log(i, name)
+//   i++
+// }while(i<=100)
+
+//  infinity loop
+
+// for (var i = 1; i > 0; i++){
+//   console.log(i, name)
+// }
+
+
+// break
+
+// for (var i = 1; i <= 100; i++){
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i, name)
+// }
+
+// var i = 1
+
+// while (i <= 100) {
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i, name)
+//   i++
+// }
+
+// var i = 1
+
+// do {
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i, name)
+//   i++
+// }while(i<=100)
+
+
+// continue
+  
+// for (var i = 1; i <= 100; i++){
+//   if (i === 50) {
+//     continue
+//   }
+//   console.log(i, name)
+// }
+
+
+// var i = 1
+
+// while (i <= 100) {
+//   if (i === 50) {
+//     i++
+//     continue
+//   }
+//   console.log(i, name)
+//   i++
+// }
+
+
+var i = 1
+
+do {
+  if (i === 50) {
+    i++
+    continue
+  }
+  console.log(i, name)
+  i++
+}while(i<=100)
