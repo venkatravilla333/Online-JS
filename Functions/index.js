2605332caaa299fdc4f43b6dec2fcd62bbff443e
konda ravilla <kondaravilla000@gmail.com>
2024-05-20 23:25:55 +0530

    functions

---
+++ Functions/index.js
@@ -0,0 +1,41 @@
+// var a = 10
+
+// var test = () => {
+//   var b = 100
+// }
+// test()
+
+//Function definition
+
+//function declaration syntax
+
+function test() {
+  
+}
+
+test()
+
+//function expression
+
+// 1) named fun exp
+
+var x = function test() { 
+
+};
+x()
+
+// 2) anonymous fun exp
+
+var y = function () { 
+
+};
+
+y()
+
+// 3) arrow fun exp
+
+var z = () => {
+  
+};
+
+z()
