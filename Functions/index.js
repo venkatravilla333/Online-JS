f08ebf9da516fde5fc6760db73dc716e2ae499ea
konda ravilla <kondaravilla000@gmail.com>
2024-05-21 22:37:30 +0530

    scope

--- Functions/index.js
+++ Functions/index.js
@@ -9,33 +9,170 @@
 
 //function declaration syntax
 
-function test() {
+// function test() {
   
-}
+// }
 
-test()
+// test()
 
 //function expression
 
 // 1) named fun exp
 
-var x = function test() { 
+// var x = function test() {
 
-};
-x()
+// };
+// x()
 
 // 2) anonymous fun exp
 
-var y = function () { 
+// var y = function () {
 
-};
+// };
 
-y()
+// y()
 
 // 3) arrow fun exp
 
-var z = () => {
+// var z = () => {
   
-};
+// };
 
-z()
+// z()
+
+//params vs arguments
+
+// var test = (a,b) => {
+//   console.log(a)
+//   console.log(b)
+// }
+// test(10,20)
+
+// default params (ES-6)
+
+// var test = (a=10, b=20) => {
+//   console.log(a)
+//   console.log(b)
+// }
+
+// test()
+
+
+//varying on of params vs arguments
+
+
+//arguments object (ES-5)
+
+// function test() {
+//   console.log(arguments)
+  // console.log(arguments[0])
+  // console.log(arguments[1])
+  // console.log(arguments[2])
+  // console.log(arguments[3])
+  // console.log(arguments[4])
+  // console.log(typeof arguments)
+  // console.log(Array.isArray(arguments))
+//   var sum = 0
+//   for (var value of arguments) {
+//     console.log(value)
+//     sum += value
+//   }
+//   return sum
+// }
+
+// console.log(test(1,2,3,4,5))
+
+
+//Rest parameter (ES-6)
+
+// var test = (...rest) => {
+  // console.log(arguments)
+//   console.log(rest)
+  // console.log(rest[0])
+  // console.log(rest[1])
+  // console.log(rest[2])
+  // console.log(rest[3])
+  // console.log(rest[4])
+  // var sum = 0
+  // for (var value of rest) {
+  //   console.log(value)
+  //   sum +=value
+  // }
+
+//   return rest.reduce((cum, cur) => {
+//     console.log(cum, cur)
+//     return cum+cur
+//   }, 0)
+
+  // return sum
+// }
+// console.log(test(1,2,3,4,5))
+
+
+//Scope
+
+
+// Global scope
+
+// storage in global
+
+var a = 1
+
+let b = 2
+
+const c = 3
+
+//access in global scope
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+function test() {
+
+  //storage in functional scope
+  var d = 4
+  let e = 5
+  const f = 6 
+
+  //access in functional scope
+  console.log(a)
+  console.log(b)
+  console.log(c)
+  console.log(d)
+  console.log(e)
+  console.log(f)
+
+  if (true) {
+
+    //storage in block scope
+    var g = 7
+    let h = 8
+    const i = 9
+    //access in block scope
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+    console.log(f)
+    console.log(g)
+    console.log(h)
+    console.log(i)
+  }
+
+  console.log(g)
+  // console.log(h)
+  // console.log(i)
+}
+
+
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+
+test()
