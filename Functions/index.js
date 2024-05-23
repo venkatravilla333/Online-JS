d2d364a9941371f904338874eb6785454812a077
konda ravilla <kondaravilla000@gmail.com>
2024-05-23 22:10:26 +0530

    hoisting

--- Functions/index.js
+++ Functions/index.js
@@ -116,56 +116,56 @@
 
 // storage in global
 
-var a = 1
+// var a = 1
 
-let b = 2
+// let b = 2
 
-const c = 3
+// const c = 3
 
-//access in global scope
+// //access in global scope
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
 
-function test() {
+// function test() {
 
-  //storage in functional scope
-  var d = 4
-  let e = 5
-  const f = 6 
+//   //storage in functional scope
+//   var d = 4
+//   let e = 5
+//   const f = 6
 
-  //access in functional scope
-  console.log(a)
-  console.log(b)
-  console.log(c)
-  console.log(d)
-  console.log(e)
-  console.log(f)
+//   //access in functional scope
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
 
-  if (true) {
+//   if (true) {
 
-    //storage in block scope
-    var g = 7
-    let h = 8
-    const i = 9
-    //access in block scope
+//     //storage in block scope
+//     var g = 7
+//     let h = 8
+//     const i = 9
+//     //access in block scope
 
-    console.log(a)
-    console.log(b)
-    console.log(c)
-    console.log(d)
-    console.log(e)
-    console.log(f)
-    console.log(g)
-    console.log(h)
-    console.log(i)
-  }
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     console.log(d)
+//     console.log(e)
+//     console.log(f)
+//     console.log(g)
+//     console.log(h)
+//     console.log(i)
+//   }
 
-  console.log(g)
-  // console.log(h)
-  // console.log(i)
-}
+//   console.log(g)
+//   // console.log(h)
+//   // console.log(i)
+// }
 
 
 // console.log(d)
@@ -175,4 +175,88 @@ function test() {
 // console.log(h)
 // console.log(i)
 
-test()
+// test()
+
+
+//Hoisting
+
+// console.log(a)
+
+// var a = 'sachin'
+// let a = 'sachin'
+
+// console.log(a)
+
+// // console.log(b)
+// // var b = 40
+// const b = 40
+// console.log(b)
+
+// console.log(c)
+// var c = true
+// console.log(c)
+
+// console.log(d)
+// var d
+// console.log(d)
+
+// console.log(e)
+// var e = null
+// console.log(e)
+
+// console.log(f)
+// var f = { name: 'kohli' }
+// console.log(f)
+
+
+// console.log(g)
+// var g = [1, 2, 3]
+// console.log(g)
+
+
+// // h()
+// // console.log(h)
+// // h()
+// let h = function test() {
+//   console.log('Named fun exp')
+// }
+// console.log(h)
+// h()
+
+// console.log(i)
+// var i = function () {
+//   console.log('Anonymous fun exp')
+// }
+// console.log(i)
+
+
+// console.log(j)
+
+// var j = () => {
+//   console.log('Arrow fun exps')
+// }
+// console.log(j)
+// //function dec syntax
+
+// console.log(declare)
+// declare()
+// function declare() {
+//   console.log('fun declaration')
+// }
+// console.log(declare)
+// declare()
+
+
+
+// console.log(x)
+// var x = 10
+// console.log(x)
+// function test() {
+//   console.log(z)
+//   z = 200
+//   console.log(z)
+//   console.log(x)
+//   var x = 100
+// }
+
+// test()
