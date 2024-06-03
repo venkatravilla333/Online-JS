8073cd8f1be28ab6a032ef7d5e4495c5c1bb7efc
konda ravilla <kondaravilla000@gmail.com>
2024-06-03 22:10:26 +0530

    arrays

---
+++ Arrays/index.js
@@ -0,0 +1,66 @@
+
+
+// var arr = [1,2,'sachin', true, undefined , null, ()=>{}, {}, []]
+// console.log(arr)
+
+// how to access array values
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+// console.log(arr[8])
+
+//how to add new values to array
+
+// var arr = [2, 3, 4]
+
+// console.log(arr)
+
+// //starting
+
+// arr.unshift(1)
+// console.log(arr)
+
+// //ending
+
+// arr.push(5)
+
+// console.log(arr)
+
+// //anywhere
+
+// arr.splice(0, 0, 'staring')
+// arr.splice(arr.length, 0, 'ending')
+// arr.splice(3, 0, 'middle')
+// console.log(arr)
+
+
+//How to remove array items
+
+var arr = [1, 2, 3, 4, 5]
+
+//staring
+
+// arr.shift()
+// console.log(arr)
+
+// ending
+
+// arr.pop()
+
+// console.log(arr)
+
+//anywhere
+
+// arr.splice(0, 1)
+// arr.splice(arr.length-1, 1)
+// arr.splice(2, 3)
+// console.log(arr)
+
+
+
