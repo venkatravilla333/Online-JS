1e703f0f158d82923762056b46d12e68b44bc8ac
konda ravilla <kondaravilla000@gmail.com>
2024-06-14 22:15:55 +0530

    async-js

---
+++ Async-Js/index.js
@@ -0,0 +1,67 @@
+
+
+//sync
+
+// console.log('hi')
+// console.log('hello')
+// console.log('bye')
+
+//sync and async
+
+
+// console.log('hi')
+// setTimeout(() => {
+//   console.log('Timer-1')
+// }, 4000)
+// setTimeout(() => {
+//   console.log('Timer-2')
+// }, 0)
+// Promise.resolve().then(() => { console.log('PR-1') }).then(() => console.log('PR-2'))
+// setTimeout(() => {
+//   console.log('Timer-3')
+// }, 5000)
+// console.log('bye')
+
+// var data
+// function fetchData(num, displayData) {
+//   setTimeout(() => {
+//     // data = { pname: 'mobile', price: 20000 }
+//     data = num*num
+//     displayData(data)
+//   }, 4000)
+
+// }
+// fetchData(2, (data1)=> {
+//   console.log(data1);
+//   fetchData(data1, (data2) => {
+//     console.log(data2)
+//     fetchData(data2, (data3) => {
+//       console.log(data3);
+//       fetchData(data3, (data4) => {
+//         console.log(data4);
+//          fetchData(data4, (data5) => {
+//            console.log(data5);
+//             fetchData(data5, (data6) => {
+//               console.log(data6);
+//                fetchData(data6, (data7) => {
+//                  console.log(data7);
+//                });
+//             });
+//          });
+//       });
+//     });
+//   } )
+// });
+
+function fetchData() {
+  
+}
+fetchData()
+
+function displayData() {
+  
+}
+
+
+
+
