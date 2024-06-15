358ca2aa805eac080899c259af17a3af39496cac
konda ravilla <kondaravilla000@gmail.com>
2024-06-15 22:23:34 +0530

    promises

--- Async-Js/index.js
+++ Async-Js/index.js
@@ -53,14 +53,65 @@
 //   } )
 // });
 
-function fetchData() {
-  
-}
-fetchData()
+//promises
 
-function displayData() {
-  
+// var data
+
+// function fetchData(num) {
+// return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       // data = { pName: 'apple', pPrice: 100 };
+//       data = num*num;
+//       res(data);
+//     }, 4000);
+//   });
+// }
+// fetchData(2).then((data1)=> {
+//   console.log(data1);
+//   return fetchData(data1)
+// }).then((data2) => {
+//   console.log(data2)
+//   return fetchData(data2);
+// }).then((data3) => {
+//   console.log(data3)
+//   return fetchData(data3)
+// }).then((data4) => {
+//   console.log(data4)
+// })
+
+//async and await
+
+var data 
+function fetchData(num) {
+ return new Promise((res, rej) => {
+    setTimeout(() => {
+      // data = { pname: 'apple', price: 100 }
+      data = num*num
+      res(data)
+    }, 4000)
+  })
 }
+
+
+async function displayData() {
+  var data1 = await fetchData(2) 
+  console.log(data1) 
+  var data2 = await fetchData(data1) 
+  console.log(data2) 
+  var data3 = await fetchData(data2) 
+  console.log(data3) 
+  var data4 = await fetchData(data3) 
+  console.log(data4) 
+  var data5 = await fetchData(data4) 
+  console.log(data5) 
+  var data6 = await fetchData(data5) 
+  console.log(data6) 
+}
+displayData()
+
+
+
+
 
 
 
