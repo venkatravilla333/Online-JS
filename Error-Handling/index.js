60b784fa64bbb12f69c9660233b1992a453993a1
konda ravilla <kondaravilla000@gmail.com>
2024-06-19 22:12:19 +0530

    error handling

---
+++ Error-Handling/index.js
@@ -0,0 +1,23 @@
+
+// console.log('start')
+// try {
+//   console.log('hi')
+//   console.log(hi) //error
+  
+// } catch (error) {
+//   console.log(error, 'hello')
+// }
+// console.log('end')
+// console.log('sachin')
+
+//custom errors
+
+// var age = 17
+
+// if (age < 18) {
+//   throw 'age must be >= 18'
+// } else {
+//   console.log('having vote')
+// }
+
+// console.log('sachin')
---
+++ Error-Types/index.js
@@ -0,0 +1,22 @@
+//types of errors in js
+
+// syntax error
+
+// var obj = {
+//   name = 'sachin'
+// }
+
+// const a
+
+//reference error
+
+// console.log(x)
+// x = 10
+
+//type error
+
+// var x = 10
+
+// x.push(100)
+// console.log(x)
+
